26abcddc445f6f4463103ee387eda719544abe76
mwikalikyalo <winniemwikali07@gmail.com>
2022-07-30 16:21:38 +0300

    Add the strip and capitalize methods

--- tip.py
+++ tip.py
@@ -1,8 +1,8 @@
 def main():
   print("Thanks for coming to Eateries.")
 #   welcome and ask if ready to pay
-  prompt1 = input('Ready to pay? ')
-  prompt1 = prompt1.capitalize()
+  prompt1 = input('Ready to pay? ').strip().capitalize()
+  
   print(prompt1)
   if prompt1== 'Yes':
       print('Do you want to tip?')
