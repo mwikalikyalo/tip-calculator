f035ef40ce9ace06c812e6bb5b77cc997bc2b0ae
mwikalikyalo <winniemwikali07@gmail.com>
2022-08-08 23:16:59 +0300

    Add question on the bill

--- tip.py
+++ tip.py
@@ -1,15 +1,19 @@
 def main():
   print("Thanks for coming to Eateries.")
-#   welcome and ask if ready to pay
+  # welcome and ask if ready to pay
   prompt1 = input('Ready to pay? ').strip().capitalize()
   
   print(prompt1)
   if prompt1== 'Yes':
-      print('Do you want to tip?')
+      print('Let us calculate the tip.')
+      prompt2= int(input("What is your bill? "))   
+      if prompt2>= 0:
+        print(prompt2)  
+           
   elif prompt1== 'No':
-        print('We will give you a minute')
+      print('We will give you a minute')
   else:
-        print('Your input was invalid.')
+      print('Your input was invalid.')
 
 if __name__ == '__main__':
     main()
